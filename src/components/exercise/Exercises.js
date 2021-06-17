2b2328a7bdffffbc07ec98c97b6408fb12055639
isabellagh <isabellagh88@gmail.com>
2021-06-17 17:35:14 -0400

    added one more exercise hard coded

--- src/components/exercise/Exercises.js
+++ src/components/exercise/Exercises.js
@@ -17,18 +17,30 @@ class Exercises extends Component {
         image_url: 'https://cdn3.omidoo.com/sites/default/files/imagecache/full_width/images/bydate/201412/cm1handedswing2.png',
         html_url: "https://breakingmuscle.com/fitness/why-one-hand-swings-are-king"   
       },
+      {
+        id: '3',
+        name: 'Suitcase lunges',
+        image_url: 'https://media.self.com/photos/59c18e0fb24b8e065cf7a75c/4:3/w_400%2Cc_limit/suitcase-lunges-jess.jpg',
+        html_url: 'https://www.self.com/gallery/beginner-kettlebell-moves'  
+      },
     ]
   }
 
   render() {  // loop through and create a list
     return (
-      <div>                   
+      <div style={exerciseStyle}>                   
         {this.state.exercises.map(exercise => (   // loop through the exercise in the state  .map takes in a function
             <ExerciseInfo key={exercise.id} exercise={exercise} />  //pass the whole "exercise/object" to the exercise prop
         ))}          
       </div>
     )
   }
+}
+
+const exerciseStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '1rem'
 }
 
 export default Exercises
