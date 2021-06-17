e16af01866b8ad7f1466aea972f8638b816c73e9
isabellagh <isabellagh88@gmail.com>
2021-06-17 17:51:09 -0400

    refactored NAVBAR class based component to function

--- src/components/exercise/ExerciseInfo.js
+++ src/components/exercise/ExerciseInfo.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class ExerciseInfo extends Component {
+const ExerciseInfo = (props) => {
 
-    render() {
-        const { name, image_url, html_url } = this.props.exercise;  // {destructuring and pulling stuff out of the object}
+  const { name, image_url, html_url } = props.exercise;  // {destructuring and pulling stuff out of the object}
 
-        return (
-            <div className="card text-center">
-                <h3>{name}</h3>
-                <img src={image_url} alt="" className="round-image" style={{ width: '150px' }}/>
-                <div>
-                    <a href={html_url} className="btn btn-dark btn-sm my-1" >More</a>
-                </div>
-            </div>
-        )
-    }
+  return (
+    <div className="card text-center">
+      <h3>{name}</h3>
+      <img src={image_url} alt="" className="round-image" style={{ width: '150px' }}/>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1" >More</a>
+      </div>
+    </div>
+  )
 }
 
 export default ExerciseInfo
