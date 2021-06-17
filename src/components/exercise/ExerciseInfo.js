83365bb80e26c22c7a289aed18a75c3a791a37f8
isabellagh <isabellagh88@gmail.com>
2021-06-16 21:37:13 -0400

    const for this.state

--- src/components/exercise/ExerciseInfo.js
+++ src/components/exercise/ExerciseInfo.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react'
 
 class ExerciseInfo extends Component {
-    constructor() {
-        super();
-        this.state = {
-            id: 'id',
-            name: '2 Handed Kettlebell Swing',
-            image_url: 'https://kettlebellsworkouts.com/wp-content/uploads/2018/08/60_double_handed_swing.png',
-            description: "Two Handed Russian Style Kettlebell swing",
-            html_url: "https://kettlebellsworkouts.com/teaching-points-for-the-kettlebell-swing/"
-        }
+    state = {
+        id: 'id',
+        name: '2 Handed Kettlebell Swing',
+        image_url: 'https://kettlebellsworkouts.com/wp-content/uploads/2018/08/60_double_handed_swing.png',
+        html_url: "https://kettlebellsworkouts.com/teaching-points-for-the-kettlebell-swing/"
     }
 
     render() {
+        const { name, image_url, html_url } = this.state  // {destructuring and pulling stuff out of the object}
+
         return (
             <div className="card text-center">
-                <h3>{this.state.name}</h3>
-                <img src={this.state.image_url} alt="" className="round-image" style={{ width: '150px' }}/>
+                <h3>{name}</h3>
+                <img src={image_url} alt="" className="round-image" style={{ width: '150px' }}/>
                 <div>
-                    <a href={this.state.html_url} className="btn btn-dark btn-sm my-1" >More</a>
+                    <a href={html_url} className="btn btn-dark btn-sm my-1" >More</a>
                 </div>
             </div>
         )
